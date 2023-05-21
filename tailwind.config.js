1d0a3885f0ffa381c652f5328fad7f10e8ad86e2
Max <agapovxd@yandex.ru>
2023-05-21 22:44:51 +0300

    +color vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -13,9 +13,13 @@ module.exports = {
       //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       // },
       colors: {
-
-      }
+        "green-primary": "#34C164",
+        "blue-primary": "#3E52C3",
+        "blue-secondary": "#E2E5F6",
+        "black-text": "#1A1C22",
+        "grey-temp": "#F5F5F7",
+      },
     },
   },
   plugins: [],
-}
+};
