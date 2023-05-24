e1a5e3533b2c62e34f780af34acaa177d8ae6115
Max <agapovxd@yandex.ru>
2023-05-25 00:21:07 +0300

    test: react-toastify lib test

--- src/components/api/HttpPost.ts
+++ src/components/api/HttpPost.ts
@@ -1,11 +1,26 @@
+"use client";
 import axios from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+// const notify = (e: any) => toast(e);
+const showToastMessage = (e: string) => {
+  toast(e, {
+    position: toast.POSITION.TOP_RIGHT,
+  });
+};
 
 const httpPost = async (url: string, data: any) => {
+  //eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter();
   await axios
     .post(url, data)
     .then((response) => {
+      if (response.status == 200) {
+        showToastMessage(response.data.message);
+      }
       console.log(response);
     })
+    .then(() => setTimeout(() => router.push("/"), 3000))
     .catch((error) => {
       console.log(error);
     });
--- src/components/major/Forms/RenterReg.tsx
+++ src/components/major/Forms/RenterReg.tsx
@@ -3,57 +3,74 @@
 import React, { FormEventHandler, useState } from "react";
 import CustomInput from "../../minor/CustomInput";
 import httpPost from "@/components/api/HttpPost";
+import { ToastContainer, toast } from "react-toastify";
+import { useRouter } from "next/router";
 
-type Props = {};
-
-const EmailRegister2 = (props: Props) => {
+const EmailRegister2: React.FC = () => {
   const [pass, setPass] = useState("");
   const [email, setEmail] = useState("");
   const [fio, setFio] = useState("");
   const [phone, setPhone] = useState("");
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    httpPost("https://rent.creatochka.cooldev.pro/api/renter/registration", {
-      fio: fio,
-      email: email,
-      password: pass,
-      phone: phone,
-    });
+    //eslint-disable-next-line react-hooks/rules-of-hooks
+    await httpPost(
+      "https://rent.creatochka.cooldev.pro/api/renter/registration",
+      {
+        fio: fio,
+        email: email,
+        password: pass,
+        phone: phone,
+      }
+    );
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CustomInput
-        type={"text"}
-        label={"Фио"}
-        valueState={[fio, setFio]}
-        trimmed={false}
-        placeholder={"Фио"}
+    <>
+      <form onSubmit={handleSubmit}>
+        <CustomInput
+          type={"text"}
+          label={"Фио"}
+          valueState={[fio, setFio]}
+          trimmed={false}
+          placeholder={"Фио"}
+        />
+        <CustomInput
+          type={"text"}
+          label={"Почта"}
+          valueState={[email, setEmail]}
+          trimmed={true}
+          placeholder={"Фио"}
+        />
+        <CustomInput
+          type={"text"}
+          label={"Номер телефона"}
+          valueState={[phone, setPhone]}
+          trimmed={true}
+          placeholder={"Фио"}
+        />
+        <CustomInput
+          type={"password"}
+          label={"Пароль"}
+          valueState={[pass, setPass]}
+          trimmed={true}
+          placeholder={"Фио"}
+        />
+        <button type="submit">asdfasd</button>
+      </form>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
       />
-      <CustomInput
-        type={"text"}
-        label={"Почта"}
-        valueState={[email, setEmail]}
-        trimmed={true}
-        placeholder={"Фио"}
-      />
-      <CustomInput
-        type={"text"}
-        label={"Номер телефона"}
-        valueState={[phone, setPhone]}
-        trimmed={true}
-        placeholder={"Фио"}
-      />
-      <CustomInput
-        type={"password"}
-        label={"Пароль"}
-        valueState={[pass, setPass]}
-        trimmed={true}
-        placeholder={"Фио"}
-      />
-      <button type="submit">asdfasd</button>
-    </form>
+    </>
   );
 };
 
