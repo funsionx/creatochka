5860c598eeee5fbb667b0060b59fce1236a7b246
Anton Arzhankin <ant.arzhankin03@gmail.com>
2023-05-28 09:37:18 +0000

    Update RenterReg.tsx

--- src/components/major/Forms/RenterReg.tsx
+++ src/components/major/Forms/RenterReg.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import React, { FormEventHandler, useState } from "react";
+import Image from "next/image";
+import pic from "../../../../public/imgs/3185942 1.png";
 import CustomInput from "../../minor/CustomInput";
 import httpPost from "@/components/api/HttpPost";
 import { ToastContainer, toast } from "react-toastify";
 import { useRouter } from "next/router";
+import { MediumButton } from "@/components/minor/Buttons";
 
 const EmailRegister2: React.FC = () => {
   const [pass, setPass] = useState("");
@@ -27,50 +30,63 @@ const EmailRegister2: React.FC = () => {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <CustomInput
-          type={"text"}
-          label={"Фио"}
-          valueState={[fio, setFio]}
-          trimmed={false}
-          placeholder={"Фио"}
+    <div className="flex justify-center items-center">
+      <div className="md:flex md:items-center md:w-full lg:w-2/3 md:mr-20 h-screen">
+        <div className="hidden md:block md:w-1/2 lg:w-2/3 m-4">
+          <Image src={pic} alt={"reg image"} className="w-full h-full object-cover" />
+        </div>
+      </div>
+      <div className="md:w-1/2 lg:w-1/3">
+        <form onSubmit={handleSubmit} className=" p-5 rounded-lg ">
+          <h2 className="text-2xl mb-6">Регистрация</h2>
+          <CustomInput
+            type="text"
+            label="Фамилия"
+            valueState={[fio, setFio]}
+            trimmed={false}
+            placeholder="Фамилия"
+          />
+          <CustomInput
+            type="text"
+            label="Email"
+            valueState={[email, setEmail]}
+            trimmed={true}
+            placeholder="Email"
+          />
+          <CustomInput
+            type="text"
+            label="Телефон"
+            valueState={[phone, setPhone]}
+            trimmed={true}
+            placeholder="Телефон"
+          />
+          <CustomInput
+            type="password"
+            label="Пароль"
+            valueState={[pass, setPass]}
+            trimmed={true}
+            placeholder="Введите пароль"
+          />
+          <MediumButton
+            type="submit"
+            twStyles="bg-blue-primary max-w-fit text-white mt-3 mb-3"
+          >
+            Зарегистрировать
+          </MediumButton>
+        </form>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
         />
-        <CustomInput
-          type={"text"}
-          label={"Почта"}
-          valueState={[email, setEmail]}
-          trimmed={true}
-          placeholder={"Фио"}
-        />
-        <CustomInput
-          type={"text"}
-          label={"Номер телефона"}
-          valueState={[phone, setPhone]}
-          trimmed={true}
-          placeholder={"Фио"}
-        />
-        <CustomInput
-          type={"password"}
-          label={"Пароль"}
-          valueState={[pass, setPass]}
-          trimmed={true}
-          placeholder={"Фио"}
-        />
-        <button type="submit">asdfasd</button>
-      </form>
-      <ToastContainer
-        position="bottom-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </>
+      </div>
+    </div>
   );
 };
 
